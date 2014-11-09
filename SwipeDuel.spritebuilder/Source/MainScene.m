19be03e6b3a5e57669687f5b7c14d07694755959
Andrew Brandt <abrandt@makeschool.com>
2014-11-09 00:14:12 -0800

    main scene stuff

--- SwipeDuel.spritebuilder/Source/MainScene.m
+++ SwipeDuel.spritebuilder/Source/MainScene.m
@@ -7,11 +7,48 @@
 //
 
 #import "MainScene.h"
+#import "Gameplay.h"
 
-@implementation MainScene
+@implementation MainScene {
+    BOOL browsing;
+    
+    CCNode *contentNode;
+    
+    CCButton *startBrowseButton;
+}
+
+- (void)onEnter {
+    [super onEnter];
+    
+    self.appDelegate = (AppController *)[[UIApplication sharedApplication] delegate];
+    startBrowseButton.userInteractionEnabled = YES;
+    browsing = NO;
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(startGame) name:@"peer-connect" object:nil];
+}
+
+- (void)onExit {
+    [self.appDelegate.manager advertiseSelf:NO];
+    [self.appDelegate.manager browse:NO];
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    [super onExit];
+}
+
+- (void)browseForDevice {
+    startBrowseButton.userInteractionEnabled = NO;
+    CCNode *dialog = [CCBReader load:@"SearchDialog" owner:self];
+    dialog.position = ccp(0, self.boundingBox.size.height);
+    
+    CCAction *present = [CCActionMoveTo actionWithDuration:0.3f position:ccp(0,0)];
+    
+    [contentNode addChild:dialog];
+    [dialog runAction:present];
+}
 
 - (void)startGame {
-
+    Gameplay *gameplay = (Gameplay *)[CCBReader loadAsScene:@"Gameplay"];
+    CCTransition *fade = [CCTransition transitionCrossFadeWithDuration:0.3f];
+    [[CCDirector sharedDirector] replaceScene:gameplay withTransition:fade];
 }
 
 @end
