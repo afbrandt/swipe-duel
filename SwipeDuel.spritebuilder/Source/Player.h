7526c3d288bcf618f7e4ed85475f3669dc11d48c
Andrew Brandt <abrandt@makeschool.com>
2014-11-09 00:31:10 -0800

    implement player, add spells

---
+++ SwipeDuel.spritebuilder/Source/Player.h
@@ -0,0 +1,13 @@
+//
+//  Player.h
+//  SwipeDuel
+//
+//  Created by Andrew Brandt on 11/9/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Player : CCNode
+
+@end
---
+++ SwipeDuel.spritebuilder/Source/Player.m
@@ -0,0 +1,13 @@
+//
+//  Player.m
+//  SwipeDuel
+//
+//  Created by Andrew Brandt on 11/9/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Player.h"
+
+@implementation Player
+
+@end
